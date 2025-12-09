4e8b27d3aadb468dd41e1f30db14ec5393b52081
phamhung0901 <phamhungtp2005@gmail.com>
2025-12-09 08:37:59 +0700

    Add Frontend Render URL to CORS

--- Backend/app/config/app.conf.js
+++ Backend/app/config/app.conf.js
@@ -9,7 +9,7 @@ module.exports = {
   },
 
   cors: {
-    origin: (process.env.CORS_ORIGIN || 'http://localhost:5173,http://localhost:5174,https://fe-thsport.vercel.app').split(','),
+    origin: (process.env.CORS_ORIGIN || 'http://localhost:5173,http://localhost:5174,https://thsports-frontend.onrender.com').split(','),
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Cache-Control', 'Pragma', 'Expires'],
